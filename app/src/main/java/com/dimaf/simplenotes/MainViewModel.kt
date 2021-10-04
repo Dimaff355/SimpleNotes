b5c340640a9c4179f01ea1d061f8361e733b7102
Dimaf <dkovalevskij@yandex.ru>
2021-10-04 22:24:28 +0300

    -черновик viewmodel

--- app/src/main/java/com/dimaf/simplenotes/MainViewModel.kt
+++ app/src/main/java/com/dimaf/simplenotes/MainViewModel.kt
@@ -1,4 +1,35 @@
 package com.dimaf.simplenotes
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel (application: Application) : AndroidViewModel(application) {
+    private val db = DatabaseNotes.getInstance(application)
+
+    private val _livedataNotes = MutableLiveData<List<EntityNote>>()
+    val livedataNotes : LiveData<List<EntityNote>> = _livedataNotes
+
+
+    fun loadNotesList() {
+        CoroutineScope((Dispatchers.IO)).launch {
+            _livedataNotes.postValue(db.daoNotes().getAllNotes())
+        }
+    }
+
+    fun removeNote(noteId : Int) {
+        CoroutineScope((Dispatchers.IO)).launch {
+            db.daoNotes().deleteNoteById(noteId)
+            loadNotesList()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+    }
 }
